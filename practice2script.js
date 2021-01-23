3acd152d3564333a00c1028a826ba1e532803e38
abhimanyubangaru <48572985+abhimanyubangaru@users.noreply.github.com>
2021-01-23 13:51:32 -0800

    testbranch

--- practice2script.js
+++ practice2script.js
@@ -1,6 +1,7 @@
 console.log("recognized file");
 function check(){
   console.log("check");
+
   var correct = 0;
   var q1 = document.quiz.question1.value;
   var result = document.getElementById("result");
@@ -10,4 +11,5 @@ function check(){
   }
   result.textContent = "You got " + correct + " right!";
   quiz.style.display = "none";
+  console.log("working");
 }
