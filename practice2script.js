dc7eaa1c958d9918cb62ae783f5ce08fb15f26f2
abhimanyubangaru <48572985+abhimanyubangaru@users.noreply.github.com>
2021-01-23 13:23:02 -0800

    I tried to start making the test quiz website, however it doesn't really work.

--- practice2script.js
+++ practice2script.js
@@ -3,3 +3,16 @@ var b = "world";
 console.log(a+ " " + b);
 console.log(a,b);
 console.log(typeof a, typeof b);
+
+
+function check(){
+  var correct = 0;
+  var q1 = document.quiz.question1.value;
+  var result = document.getElementById('result');
+  var quiz = document.getElementById('quiz');
+  if(q1 == "Abhi"){
+    c++;
+  }
+  result.textContent = '${correct}';
+  quiz.style.display = "none";
+}
