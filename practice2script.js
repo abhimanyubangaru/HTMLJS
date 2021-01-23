3413f7e4b4499f2f99364d6ebd519fb6d4fdacac
abhimanyubangaru <48572985+abhimanyubangaru@users.noreply.github.com>
2021-01-23 13:36:20 -0800

    Finished one question. Add on if you want.

--- practice2script.js
+++ practice2script.js
@@ -1,11 +1,13 @@
+console.log("recognized file");
 function check(){
+  console.log("check");
   var correct = 0;
   var q1 = document.quiz.question1.value;
-  var result = document.getElementById('result');
-  var quiz = document.getElementById('quiz');
+  var result = document.getElementById("result");
+  var quiz = document.getElementById("quiz");
   if(q1 == "Abhi"){
-    c++;
+    correct++;
   }
-  result.textContent = '${correct}';
+  result.textContent = "You got " + correct + " right!";
   quiz.style.display = "none";
 }
